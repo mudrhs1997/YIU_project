402a0efbe07a9eae14ab2f685b44a0f0c528b97e
mngon <77499260+mudrhs1997@users.noreply.github.com>
2021-12-12 22:29:18 +0900

    미로예시, 최종본

--- maze.cpp
+++ maze.cpp
@@ -2,22 +2,23 @@
 #include <stack>
 #include <cstdio>
 #include <utility>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <unistd.h>
+#include <vector>
 #define MAX_LIST_SIZE 100;
 using namespace std;
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
-bool visited[6][6];
-int maze[6][6] =
-{   {1,1,1,1,1,1},
-    {0,0,0,0,0,1},
-    {1,0,1,0,1,1},
-    {1,1,1,0,0,0},
-    {1,1,1,0,1,1},
-    {1,1,1,1,1,1}
-};
-
-
+int cnt_x = 0;
+int cnt_y = 0;
+bool visited[50][50];
+vector<int> maze_ex;
+vector<vector<int> > maze;
+string result = "";
 
 class Node {
     Node* link;
@@ -89,15 +90,15 @@ public:
 };
 
 LinkedStack st;
-pair<int, int> ex = make_pair(3, 5);
 
-void search(pair<int, int> start) {
+void search(pair<int, int> start, pair<int, int> exit) {
     visited[start.first][start.second] = true;
     maze[start.first][start.second] = 2;
+    result += " (" + to_string(start.first) + "," + to_string(start.second) + ")";
     for (int i = 0; i < 4; i++) {
         int point_x = start.first+dx[i];
         int point_y = start.second+dy[i];
-        if (0 <= point_x && point_x < 6 && 0 <= point_y && point_y < 6) {
+        if (0 <= point_x && point_x < cnt_x && 0 <= point_y && point_y < cnt_y) {
             if (visited[point_x][point_y] == false) {
                 st.push(new Node(point_x,point_y));
             }
@@ -106,38 +107,72 @@ void search(pair<int, int> start) {
     printf("Stack : ");
     st.display();
     printf("\n");
-    if (start == ex) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+    cout << "Route :" << result << endl;
+    if (start == exit) {
+        for (int i = 0; i < cnt_x; i++) {
+            for (int j = 0; j < cnt_y; j++) {
                 printf("%d ",maze[i][j]);
             }
             printf("\n");
         }
         printf("\narrive!\n");
+        cout << "Final Route :" << result << endl;
     } else if (!st.isEmpty()) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < cnt_x; i++) {
+            for (int j = 0; j < cnt_y; j++) {
                 printf("%d ",maze[i][j]);
             }
             printf("\n");
         }
+        sleep(1);
         printf("\n");
         Node *n = st.pop();
         pair<int, int> next = n -> getPoint();
-        search(next);
+        search(next, exit);
     }
 }
 
 
-int main() {
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
+int main(int argc, char *argv[]) {
+    ifstream ifile;
+    char line[50];
+    
+    pair<int, int> start;
+    pair<int, int> exit;
+    
+    ifile.open(argv[1]);
+    if (ifile.is_open()) {
+        while(ifile.getline(line,sizeof(line))) {
+            cnt_y = 0;
+            for(int i = 0; i < 50; i++) {
+                if(line[i] == 'x') {
+                    maze_ex.push_back(0);
+                } else {
+                    maze_ex.push_back(line[i]-'0');
+                }
+                if('0'==line[i]||line[i]=='1'||line[i]=='e'||line[i]=='x') {
+                    if (line[i] == 'e') {
+                        start = make_pair(cnt_x, cnt_y);
+                    } else if (line[i] == 'x') {
+                        exit = make_pair(cnt_x, cnt_y);
+                    }
+                    cnt_y++;
+                }
+            }
+            cnt_x++;
+            maze.push_back(maze_ex);
+            maze_ex.clear();
+        }
+    }
+    ifile.close();
+    for (int i = 0; i < cnt_x; i++) {
+        for (int j = 0; j < cnt_y; j++) {
             if (maze[i][j] == 1) {
                 visited[i][j] = true;
             }
         }
     }
-    search(make_pair(1, 0));
+    search(start, exit);
 }
 
 //class LinkedQueue {
