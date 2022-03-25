74601653bcaaa9d29337f2129f89b30cee524182
mudrhs1997 <mudrhs1997@daum.net>
2022-03-25 22:41:57 +0900

    update

--- 3-1) 데이터베이스/OurTeam/OurTeam/TeamMainViewController.swift
+++ 3-1) 데이터베이스/OurTeam/OurTeam/TeamMainViewController.swift
@@ -13,7 +13,6 @@ class TeamMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "?"
         // Do any additional setup after loading the view.
     }
 
