3cc198486b2eccd1e55476070f4b1b50e209edf8
mudrhs1997 <mudrhs1997@daum.net>
2022-04-04 23:29:32 +0900

    to scrollview

--- 3-1) 데이터베이스/OurTeam/OurTeam/ViewController.swift
+++ 3-1) 데이터베이스/OurTeam/OurTeam/ViewController.swift
@@ -23,6 +23,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
